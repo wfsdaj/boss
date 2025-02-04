7fbf8c2bbcc9b603957c4809dd6603c836ad0802
wfdaj <wfdaj@qq.com>
2025-02-04 21:43:27 +0800

    #

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ header("Referrer-Policy: same-origin");
 header('X-Frame-Options: SAMEORIGIN');
 header('X-Powered-By:');
 
-define('TRACE', true);  // 追踪模式
+// define('TRACE', true);  // 追踪模式
 define('DEBUG', true);  // 调试模式
 define('SHOW_ERROR', true); // 是否展示错误信息
 define('ENABLE_SESSION', true); // 启用 session
